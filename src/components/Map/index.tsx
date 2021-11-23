ef78ee873d711f1f387d46ffc21768614dbffd78
andresgutgon <andresgutgon@gmail.com>
2021-11-23 16:13:56 +0100

    Show clusters when there are too many markers at that zoom level for that area

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,6 +1,10 @@
+// Styles from Leaflet and plugins
 import 'leaflet/dist/leaflet.css';
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 
 import * as L from 'leaflet'
+import 'leaflet.markercluster'
 import Script from 'next/script'
 
 import { CommunityProvider, useMap } from '@maps/components/CommunityProvider'
@@ -26,7 +30,7 @@ const MapWrapper = () => {
           var map = window.L.map('map')
 
           // Set Center of map and zoom level
-          map.setView(center, 14)
+          map.setView(center, 9)
 
           // Tiles provider
           var titleProvider = window.L.tileLayer(
@@ -42,18 +46,34 @@ const MapWrapper = () => {
           )
           titleProvider.addTo(map)
 
+          // Set the right icon URLs
           delete L.Icon.Default.prototype._getIconUrl;
           window.L.Icon.Default.mergeOptions({
             iconUrl: '/marker-icon.png',
             iconRetinaUrl: '/marker-icon-2x.png',
             shadowUrl: '/marker-shadow.png'
           })
-          // Add a humble marker
-          const markers = ${JSON.stringify(markers)}
-          markers.map((marker) => {
-            var marker = window.L.marker([marker.lat, marker.long])
-            marker.addTo(map)
-          })
+
+          // Marker clusters
+          var markersCluster = window.L.markerClusterGroup({
+            showCoverageOnHover: false,
+            disableClusteringAtZoom: 12,
+            removeOutsideVisibleBounds: true
+          });
+
+          const markersData = ${JSON.stringify(markers)}
+          const markerList = markersData.map((marker) =>
+            window.L.marker(
+              [marker.lat, marker.long],
+              { title: marker.name }
+            )
+          )
+
+          // Add markers to the cluster
+          markersCluster.addLayers(markerList)
+
+          // Add cluster to the map
+          map.addLayer(markersCluster)
         `,
       }}
     />
