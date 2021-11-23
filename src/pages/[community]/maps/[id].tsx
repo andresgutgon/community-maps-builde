05dd09e307ff42eab8548e9d82dab68ab6c76f4e
andresgutgon <andresgutgon@gmail.com>
2021-11-23 14:44:42 +0100

    Loading fallback

--- src/pages/[community]/maps/[id].tsx
+++ src/pages/[community]/maps/[id].tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import dynamic from 'next/dynamic'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
@@ -5,13 +6,21 @@ import { useRouter } from 'next/router'
 const MapRoute = () => {
   const Map = dynamic(
     () => import('@maps/components/Map'),
-    { ssr: false } // This line is important. It's what prevents server-side render
+    {
+      // This line is important. It's what prevents server-side render
+      ssr: false,
+      loading: () =>
+        <div className='absolute inset-0 w-full flex items-center justify-center text-gray-400'>
+          Loading...
+        </div>
+    }
   )
   const router = useRouter()
 
   if (!router.query.community || !router.query.id) {
     return null
   }
+
 
   const { community, id } = router.query
   return (
