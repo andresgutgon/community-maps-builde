16e8383a7d948dbff1d00e4a3d434299db35e955
andresgutgon <andresgutgon@gmail.com>
2021-11-24 12:56:24 +0100

    Reactify Leaflet by using react-leaflet

--- src/components/CommunityProvider/useTile.ts
+++ src/components/CommunityProvider/useTile.ts
@@ -1,8 +1,26 @@
+import { useMemo } from 'react'
 import { TILES } from '@maps/types/index'
-import type { Config, Tile } from '@maps/types/index'
+import type { MapAttribution, Config, Tile } from '@maps/types/index'
 
-const useTile = (config: Config | null): Tile | null => {
-  return TILES[config?.theme?.tileStyle] || TILES.osm
+type ReturnType = {
+  url: string
+  minZoom: number
+  maxZoom: number
+  attribution: string
+}
+const useTile = (config: Config | null): ReturnType => {
+  const style = config?.theme?.tileStyle
+  return useMemo<ReturnType>((): ReturnType => {
+      const { attributions, url, minZoom, maxZoom } = TILES[style] || TILES.osm
+      const attribution = attributions.map((attribution: MapAttribution) => {
+        if (!attribution.link) return attribution.linkText
+        return `<a href=${attribution.link} target='_blank'>${attribution.linkText}</a>`
+      }).join(' | ')
+
+      return { url, minZoom, maxZoom, attribution }
+    },
+    [style]
+  )
 }
 
 export default useTile
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,93 +1,65 @@
+import { useEffect, useState, useRef } from 'react'
+
+import * as Leaflet from 'leaflet'
+import 'leaflet.markercluster'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import MarkerClusterGroup from 'react-leaflet-markercluster'
+
 // Styles from Leaflet and plugins
 import 'leaflet/dist/leaflet.css';
 import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 
-import * as L from 'leaflet'
-import 'leaflet.markercluster'
-import Script from 'next/script'
-
 import { CommunityProvider, useMap } from '@maps/components/CommunityProvider'
-import type { MapAttribution, Marker } from '@maps/types/index'
+import type { MapAttribution, Marker as MarkerType } from '@maps/types/index'
 import useTile from '@maps/components/CommunityProvider/useTile'
 
-
-window.L = L
+// TODO: Custom icons
+delete (Leaflet.Icon.Default.prototype as any)._getIconUrl
+Leaflet.Icon.Default.mergeOptions({
+  iconUrl: '/marker-icon.png',
+  iconRetinaUrl: '/marker-icon-2x.png',
+  shadowUrl: '/marker-shadow.png'
+})
 
 const MapWrapper = () => {
+  const [map, setMap] = useState<Leaflet.Map>(null)
+  const clusterRef = useRef<MarkerClusterGroup>(null)
   const { loading, config, markers } = useMap()
   const tile = useTile(config)
+  useEffect(() => {
+    if (!map) return;
 
+    map.fitBounds(clusterRef.current.getBounds());
+  }, [map])
   if (loading) return null
 
   return (
-    <Script
-      id='getStarted'
-      dangerouslySetInnerHTML={{
-        __html: `
-          // var center = [41.382894, 2.177432]
-          // Leaflet initialization
-          var map = window.L.map('map')
-
-          // Set Center of map and zoom level
-          // map.setView(center, 9)
-
-          // Tiles provider
-          var titleProvider = window.L.tileLayer(
-            "${tile.url}",
-            {
-              minZoom: ${tile.minZoom},
-              maxZoom: ${tile.maxZoom},
-              attribution: "${tile.attributions.map((attribution: MapAttribution) => {
-                if (!attribution.link) return attribution.linkText
-                return `<a href=${attribution.link} target='_blank'>${attribution.linkText}</a>`
-              }).join(' | ')}"
-            }
+    <MapContainer
+      whenCreated={setMap}
+      className='bg-gray-50 w-screen h-screen'
+    >
+      <TileLayer {...tile} />
+      <MarkerClusterGroup
+        ref={clusterRef}
+        showCoverageOnHover={false}
+        disableClusteringAtZoom={12}
+        removeOutsideVisibleBounds={true}
+      >
+        {markers.map(({ lat, long, name: title }: MarkerType, index: number) => {
+          const latLong = { lat: parseFloat(lat), lng: parseFloat(long) }
+          // Extend LatLngBounds with coordinates
+          // bounds.extend(latLong)
+          return (
+            <Marker key={index} position={latLong}>
+              <Popup>
+                {`\`{ lat: '\$\{lat\}', long: '\$\{long\}\', name: '\$\{title\}' }\``}
+              </Popup>
+            </Marker>
           )
-          titleProvider.addTo(map)
-
-          // Set the right icon URLs
-          delete L.Icon.Default.prototype._getIconUrl;
-          window.L.Icon.Default.mergeOptions({
-            iconUrl: '/marker-icon.png',
-            iconRetinaUrl: '/marker-icon-2x.png',
-            shadowUrl: '/marker-shadow.png'
-          })
-
-          // Marker clusters
-          var markersCluster = window.L.markerClusterGroup({
-            showCoverageOnHover: false,
-            disableClusteringAtZoom: 12,
-            removeOutsideVisibleBounds: true
-          });
-
-          const fitBoundsPadding = [${markers.length > 20 ? '1, 1' : '100, 100'}]
-          const markersData = ${JSON.stringify(markers)}
-
-          // Instantiate LatLngBounds object
-          var bounds = window.L.latLngBounds()
-
-          const markerList = markersData.map(({ lat, long, name: title }) => {
-            const latLong = [lat, long]
-
-            // Extend LatLngBounds with coordinates
-            bounds.extend(latLong)
-
-            const marker = window.L.marker(latLong, { title })
-            marker.bindPopup(\`{ lat: '\$\{lat\}', long: '\$\{long\}\', name: '\$\{title\}' }\`)
-            return marker
-          })
-
-          map.fitBounds(bounds, { padding: fitBoundsPadding})
-
-          // Add markers to the cluster
-          markersCluster.addLayers(markerList)
-
-          // Add cluster to the map
-          map.addLayer(markersCluster)
-        `,
-      }}
-    />
+        })}
+      </MarkerClusterGroup>
+    </MapContainer>
   )
 }
 
@@ -98,7 +70,6 @@ type Props = {
 const Map = ({ community, mapId }: Props) => {
   return (
     <CommunityProvider community={community} mapId={mapId}>
-      <div id='map' className='bg-gray-50 w-screen h-screen' />
       <MapWrapper />
     </CommunityProvider>
   )
