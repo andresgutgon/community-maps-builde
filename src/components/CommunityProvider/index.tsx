b778caa7c9e934b7913c01be035a0df285bd9c74
andresgutgon <andresgutgon@gmail.com>
2021-12-17 14:06:16 +0100

    Implement iframe communication We want to embed maps in an iframe. We also want to get URL params from host page. For security reasons the way of doing this is with https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage

--- src/components/CommunityProvider/index.tsx
+++ src/components/CommunityProvider/index.tsx
@@ -1,8 +1,59 @@
 import { ReactNode, createContext, useEffect, useState, useContext } from 'react'
+import { useRouter } from 'next/router'
 import { FormattedMessage } from 'react-intl'
 
 import { Place, Config } from '@maps/types/index'
 import LoadingMap from '@maps/components/LoadingMap'
+
+
+function useHostWindow ():  null | Window {
+  try {
+    const isIframe = window.self !== window.parent
+    if (!isIframe) return null
+    return window.parent
+  } catch (e) {
+    return null
+  }
+}
+
+const DEFAULT_URL_PARAMS = { placeSlug: null }
+type UrlParam = {
+  placeSlug: string | null
+}
+type ReturnTypeQueryString = {
+  loadingUrlParams: boolean
+  urlParams: UrlParam
+}
+const useQueryString = (): ReturnTypeQueryString => {
+  const [loadingUrlParams, setLoading] = useState(true)
+  const [urlParams, setUrlParams] = useState<null | UrlParam>(DEFAULT_URL_PARAMS)
+  const parentWindow = useHostWindow()
+
+  useEffect(() => {
+    function handleMessage (event: MessageEvent) {
+      if (event.data.type === 'SET_PARAMS_FROM_PARENT') {
+        setUrlParams({ placeSlug: event.data.queryParams.mapPlace })
+        setLoading(false)
+      }
+    }
+
+    if (parentWindow) {
+      window.addEventListener('message', handleMessage)
+      // Ask parent window about their URL params
+      parentWindow.postMessage({ type: 'GET_URL' }, '*')
+    } else {
+      const queryString = window.location.search
+      const params = new URLSearchParams(queryString)
+      setUrlParams({ placeSlug: params.get('mapPlace') })
+      setLoading(false)
+    }
+    return () => {
+      window.removeEventListener('message', handleMessage)
+    }
+  }, [parentWindow])
+
+  return { loadingUrlParams, urlParams }
+}
 
 interface ContextProps {
   places: Array<Place>;
@@ -22,10 +73,16 @@ type ProviderProps = {
 }
 
 export const CommunityProvider = ({ community, mapId, children }: ProviderProps) => {
+  const { loadingUrlParams, urlParams } = useQueryString()
+  console.log('placeSlug', urlParams.placeSlug)
+
   const [loading, setLoading] = useState(true)
   const [config, setConfig] = useState(null)
   const [places, setPlaces] = useState([])
   useEffect(() => {
+    // Wait for parent host page to return URL info
+    if (loadingUrlParams) return
+
     async function loadData () {
       // Places are async
       fetch(`/api/${community}/maps/${mapId}/places`)
@@ -39,7 +96,7 @@ export const CommunityProvider = ({ community, mapId, children }: ProviderProps)
       setLoading(false)
     }
     loadData()
-  }, [community, mapId])
+  }, [community, mapId, loadingUrlParams])
   return (
     <CommunityContext.Provider value={{ loading, places, config }}>
       {loading ? <LoadingMap /> : children}
