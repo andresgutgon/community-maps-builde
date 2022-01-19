6eda9f1d3e47fc493b80bf0e8376bb3367ff4277
andresgutgon <andresgutgon@gmail.com>
2022-01-19 14:45:40 +0100

    Add prettier to the project As described in their documentation https://prettier.io/docs/en/install.html With CI, pre-commit and adding a vim plugin to change on save

--- next.config.js
+++ next.config.js
@@ -2,5 +2,16 @@ module.exports = {
   i18n: {
     locales: ['es', 'ca'],
     defaultLocale: 'es'
+  },
+  eslint: {
+    dirs: [
+      'pages',
+      'components',
+      'lib',
+      'hooks',
+      'data',
+      'styles',
+      'types'
+    ]
   }
 }
