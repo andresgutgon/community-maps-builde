f7e697d84244ee82a7a2430625644a3ec3289018
konykon <konykon@riseup.net>
2022-03-23 11:09:19 +0100

    Fix lint

--- src/components/CustomJsonForms/Renderers/controls/Image/index.tsx
+++ src/components/CustomJsonForms/Renderers/controls/Image/index.tsx
@@ -1,40 +1,25 @@
-import {
-  rankWith,
-  RankedTester,
-  uiTypeIs,
-  ControlProps
-} from '@jsonforms/core'
+import { rankWith, RankedTester, uiTypeIs, ControlProps } from '@jsonforms/core'
 import { withJsonFormsControlProps } from '@jsonforms/react'
 import {
   withVanillaControlProps,
   VanillaRendererProps
 } from '@jsonforms/vanilla-renderers'
 
-
-const isImage = (
-  uiTypeIs('Control')
-)
+const isImage = uiTypeIs('Control')
 
 export const imageTester: RankedTester = rankWith(10, isImage)
 
-
 type Props = ControlProps & VanillaRendererProps
-const ImageControl = ({
-  uischema,
-  visible,
-  classNames
-}: Props) => {
+const ImageControl = ({ uischema, visible, classNames }: Props) => {
   const src = uischema?.options?.src
 
   if (!visible) return null
 
   return (
     <div className={classNames.wrapper}>
-      <img src={src}/>
+      <img src={src} />
     </div>
   )
 }
 
-export default withVanillaControlProps(
-  withJsonFormsControlProps(ImageControl)
-)
+export default withVanillaControlProps(withJsonFormsControlProps(ImageControl))
